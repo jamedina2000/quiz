8d7eec88c7fc7ad6a65e175e7a4596a63f963c46
jamedina2000 <foros2014@yahoo.es>
2015-07-26 11:39:40 +0200

    Moderación de Comentarios

--- routes/index.js
+++ routes/index.js
@@ -12,6 +12,7 @@ router.get('/', function(req, res) {
 
 //Autoload de camandos con :quizId
 router.param('quizId', quizController.load);  //autoload :quizId
+router.param('commentId', commentController.load); //autoload :commentId
 
 // Definicion de rutas de sesion
 router.get('/login', sessionController.new); //formulario login
@@ -29,7 +30,9 @@ router.put('/quizes/:quizId(\\d+)', sessionController.loginRequired, quizControl
 router.delete('/quizes/:quizId(\\d+)', sessionController.loginRequired, quizController.destroy);
 
 router.get('/quizes/:quizId(\\d+)/comments/new', commentController.new); 
-router.post('/quizes/:quizId(\\d+)/comments', commentController.create); 
+router.post('/quizes/:quizId(\\d+)/comments', commentController.create);
+router.get('/quizes/:quizId(\\d+)/comments/:commentId(\\d+)/publish', 
+			sessionController.loginRequired, commentController.publish); 
 
 
 // Definición ruta autor
